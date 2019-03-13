55c47bbbdac4d9acd5213526bdc675a82b38a0e0
i_whammy <yas.iwami@gmail.com>
2019-03-13 15:33:16 +0900

    added unit test

--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -1,7 +1,17 @@
 (ns fizzbuzz.core-test
-  (:require [clojure.test :refer :all]
-            [fizzbuzz.core :refer :all]))
+  (:require [clojure.test :as t]
+            [fizzbuzz.core :as sut]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(t/deftest fizzbuzz-test
+  (t/testing "when you post multiple of 3, it says fizz"
+    (t/is (= "fizz" (sut/fizz-buzz 3))))
+  (t/testing "when you post multiple of 5, it says buzz"
+    (t/is (= "buzz" (sut/fizz-buzz 5))))
+  (t/testing "when you post multiple of 15, it says fizz-buzz"
+    (t/is (= "fizz-buzz" (sut/fizz-buzz 15))))
+  (t/testing "when you post non-multiple of 3,5,15, it says yournum"
+    (t/is (= 2 (sut/fizz-buzz 2)))))
+
+(t/deftest fizzbuzz-test-irregular-case
+  (t/testing "when you get not-number, it says Panicked"
+    (t/is (= "Panicked!" (sut/fizz-buzz "hoge")))))
